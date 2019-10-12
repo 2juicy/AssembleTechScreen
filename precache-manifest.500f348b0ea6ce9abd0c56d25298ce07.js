b4165ac0c3fe34a48579452d726c7641c03e1035
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-12 13:32:10 -0700

    Updates

---
+++ precache-manifest.500f348b0ea6ce9abd0c56d25298ce07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bb3f96e5bcc2579471031a7cc04837",
+    "url": "/AssembleTechScreen/index.html"
+  },
+  {
+    "revision": "def9fd4aad5fdcdd55e1",
+    "url": "/AssembleTechScreen/static/css/main.652a975c.chunk.css"
+  },
+  {
+    "revision": "1447e85cb5d7b79947ee",
+    "url": "/AssembleTechScreen/static/js/2.e626a063.chunk.js"
+  },
+  {
+    "revision": "def9fd4aad5fdcdd55e1",
+    "url": "/AssembleTechScreen/static/js/main.fdc79b9f.chunk.js"
+  },
+  {
+    "revision": "2e775b33ea70fbbbc78b",
+    "url": "/AssembleTechScreen/static/js/runtime-main.7c5f863b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AssembleTechScreen/precache-manifest.fae89944202ebb32be4e6605d1bd8f93.js"
+  "/AssembleTechScreen/precache-manifest.500f348b0ea6ce9abd0c56d25298ce07.js"
 );
 
 self.addEventListener('message', (event) => {
