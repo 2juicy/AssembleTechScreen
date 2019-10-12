6baebced84def2c81dabc0047f1112b860beac27
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-11 20:03:10 -0700

    final page completed

--- src/pages/complete/Complete.js
+++ src/pages/complete/Complete.js
@@ -4,11 +4,19 @@ import "./Complete.css";
 export default function Complete() {
   return (
     <section id="order-complete-page">
-      <h2>Order Complete</h2>
+      <div className="complete-title">
+        <h2>Order Complete</h2>
+      </div>
 
-      <a className="button right" href="/">
-        Go Home
-      </a>
+      <h4 className="order-text center">
+        Hooray! Way to order those products.
+      </h4>
+
+      <div className="center">
+        <a className="button" href="/">
+          Go Home
+        </a>
+      </div>
     </section>
   );
 }
