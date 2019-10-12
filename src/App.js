0b3392dfd0c43c78e3c89bccb91997bbb0136272
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-11 20:56:39 -0700

    gh pages routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Billing from "./pages/billing/Billing";
 import Complete from "./pages/complete/Complete";
@@ -8,11 +8,11 @@ import "./App.css";
 
 export default function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
       <Route path="/billing" component={Billing} />
       <Route path="/complete" component={Complete} />
-    </Router>
+    </>
   );
 }
