f5301dad80d06ddc4bf4c1d01b5c0a8670902715
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-11 18:06:18 -0700

    initial routing setup

---
+++ src/pages/billing/Billing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Billing() {
+  return (
+    <div>
+      <h2>Billing</h2>
+    </div>
+  );
+}
---
+++ src/pages/complete/Complete.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Complete() {
+  return (
+    <div>
+      <h2>Order Complete</h2>
+    </div>
+  );
+}
